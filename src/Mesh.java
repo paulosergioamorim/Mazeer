e2fef04442579eb327f13fe2199e32945d587ba8
Paulo Sergio <pamorimmonico@gmail.com>
2021-08-10 08:44:51 -0300

    update

--- src/Mesh.java
+++ src/Mesh.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Mesh {
-    public int[][] field;
+    public Integer[][] field;
     public final int size;
 
     public final List<Point> points = new ArrayList<>();
@@ -12,7 +12,7 @@ public class Mesh {
 
     public Mesh(int size) {
         this.size = size;
-        field = new int[size][size];
+        field = new Integer[size][size];
 
         this.setBounds();
     }
@@ -34,33 +34,64 @@ public class Mesh {
         Point actual = new Point(1,1);
         Point end = new Point(size-2,size-2);
 
+
         while (true) {
+
             if (actual.equals(end)) return;
+
+            double north = disperse(new Point(actual.x-1,actual.y));
+            double south = disperse(new Point(actual.x+1,actual.y));
+            double rigth = disperse(new Point(actual.x,actual.y+1));
+            double left = disperse(new Point(actual.x,actual.y-1));
+
+            double max = Double.max(Double.max(north, south), Double.max(rigth, left));
+
+            if (max == north) {
+                actual = new Point(actual.x-1,actual.y);
+            }
+            else if (max == south) {
+                actual = new Point(actual.x+1,actual.y);
+            }
+            else if (max == rigth) {
+                actual = new Point(actual.x,actual.y+1);
+            }
+            else if (max == left) {
+                actual = new Point(actual.x,actual.y-1);
+            } path.add(actual);
+
+        }
+    }
+
+    public double disperse(Point init) {
+        Point end = new Point(size-2,size-2);
+
+        for (int i = 0; i < 10 ; i++) {
+            if (init.equals(end)) {
+                System.out.println("End");
+                return 1 / (init.distance(end) + 1);
+            }
 
             Random random = new Random();
             List<Point> candidates = new ArrayList<>();
 
-            Point north = new Point(actual.x,actual.y+1);
-            Point south = new Point(actual.x,actual.y-1);
-            Point rigth = new Point(actual.x+1, actual.y);
-            Point left = new Point(actual.x-1, actual.y);
+            Point north = new Point(init.x-1,init.y);
+            Point south = new Point(init.x+1,init.y);
+            Point rigth = new Point(init.x, init.y+1);
+            Point left = new Point(init.x, init.y-1);
 
-            if (field[north.x][north.y] != 1) candidates.add(north);
-            if (field[south.x][south.y] != 1) candidates.add(south);
-            if (field[rigth.x][rigth.y] != 1) candidates.add(rigth);
-            if (field[left.x][left.y] != 1) candidates.add(left);
-
-            candidates.removeIf(path::contains);
+            if (field[north.x][north.y] == null) candidates.add(north);
+            if (field[south.x][south.y] == null) candidates.add(south);
+            if (field[rigth.x][rigth.y] == null) candidates.add(rigth);
+            if (field[left.x][left.y] == null) candidates.add(left);
 
             if (candidates.isEmpty()) {
-                return;
+                return 1 / (init.distance(end) + 1);
             } else {
                 int result = random.nextInt(candidates.size());
-                actual = candidates.get(result);
-                field[actual.x][actual.y] = 2;
-                path.add(actual);
+                init = candidates.get(result);
             }
         }
+        return 1 / (init.distance(end) + 1);
     }
 
     public String pathToString() {
