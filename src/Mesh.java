71083ae4ae3bf9c12f8ab67f60bd600c30c6e2ef
Paulo Sergio <pamorimmonico@gmail.com>
2021-08-10 08:49:56 -0300

    update

--- src/Mesh.java
+++ src/Mesh.java
@@ -34,23 +34,21 @@ public class Mesh {
         Point actual = new Point(1,1);
         Point end = new Point(size-2,size-2);
 
-
         while (true) {
-
             if (actual.equals(end)) return;
 
-            double north = disperse(new Point(actual.x-1,actual.y));
-            double south = disperse(new Point(actual.x+1,actual.y));
+            double north = disperse(new Point(actual.x+1,actual.y));
+            double south = disperse(new Point(actual.x-1,actual.y));
             double rigth = disperse(new Point(actual.x,actual.y+1));
             double left = disperse(new Point(actual.x,actual.y-1));
 
             double max = Double.max(Double.max(north, south), Double.max(rigth, left));
 
             if (max == north) {
-                actual = new Point(actual.x-1,actual.y);
+                actual = new Point(actual.x+1,actual.y);
             }
             else if (max == south) {
-                actual = new Point(actual.x+1,actual.y);
+                actual = new Point(actual.x-1,actual.y);
             }
             else if (max == rigth) {
                 actual = new Point(actual.x,actual.y+1);
@@ -74,8 +72,8 @@ public class Mesh {
             Random random = new Random();
             List<Point> candidates = new ArrayList<>();
 
-            Point north = new Point(init.x-1,init.y);
-            Point south = new Point(init.x+1,init.y);
+            Point north = new Point(init.x+1,init.y);
+            Point south = new Point(init.x-1,init.y);
             Point rigth = new Point(init.x, init.y+1);
             Point left = new Point(init.x, init.y-1);
 
@@ -83,6 +81,14 @@ public class Mesh {
             if (field[south.x][south.y] == null) candidates.add(south);
             if (field[rigth.x][rigth.y] == null) candidates.add(rigth);
             if (field[left.x][left.y] == null) candidates.add(left);
+
+            candidates.removeIf(point -> (
+                    point.x == 0
+                            || point.x == size - 1
+                            || point.y == 0
+                            || point.y == size - 1
+            ));
+
 
             if (candidates.isEmpty()) {
                 return 1 / (init.distance(end) + 1);
